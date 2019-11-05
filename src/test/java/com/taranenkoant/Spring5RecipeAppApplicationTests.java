e7345a63093ed37fce441ea887665cd77433f349
Антон Тараненко <taranenkoant@gmail.com>
2019-11-05 13:05:17 +0300

    initial commit

---
+++ src/test/java/com/taranenkoant/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taranenkoant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
