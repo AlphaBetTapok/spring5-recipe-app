d21ade99d82f870b7f4d99d5f58241bace529b27
Антон Тараненко <taranenkoant@gmail.com>
2019-11-18 17:47:09 +0300

    Optional findByDescription

--- src/main/java/com/taranenkoant/controllers/IndexController.java
+++ src/main/java/com/taranenkoant/controllers/IndexController.java
@@ -1,13 +1,34 @@
 package com.taranenkoant.controllers;
 
+import com.taranenkoant.domain.Category;
+import com.taranenkoant.domain.UnitOfMeasure;
+import com.taranenkoant.repositories.CategoryRepository;
+import com.taranenkoant.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"", "/", "index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("id is: " + categoryOptional.get().getId());
+        System.out.println("UOM id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
