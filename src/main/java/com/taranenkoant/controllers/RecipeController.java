71a4797c0493eda74572b07d5489e07de5be493e
Антон Тараненко <taranenkoant@gmail.com>
2020-01-28 11:27:34 +0300

    add page 404

--- src/main/java/com/taranenkoant/controllers/RecipeController.java
+++ src/main/java/com/taranenkoant/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package com.taranenkoant.controllers;
 
 import com.taranenkoant.commands.RecipeCommand;
+import com.taranenkoant.exceptions.NotFoundException;
 import com.taranenkoant.service.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Slf4j
 @Controller
@@ -55,5 +55,13 @@ public class RecipeController {
         recipeService.deleteById(Long.valueOf(id));
 
         return "redirect:/";
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(){
+        log.error("Handling not found exception");
+        ModelAndView modelAndView = new ModelAndView("404error");
+        return  modelAndView;
     }
 }
